7c1e1cbfa465a0372f418f384d67692c34ba3c93
ramadhanmuliaf <ramadhanfajri2001@gmail.com>
2022-12-19 20:50:20 +0700

    sistem e-spp

---
+++ resources/views/dashboard/siswa/rules.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.dashboard-siswa')
+
+@section('breadcrumb')
+   <li class="breadcrumb-item">Dashboard</li>
+   <li class="breadcrumb-item active">Tata Cara Pembayaran</li>
+@endsection
+
+@section('content')
+
+   <div class="row">
+      <div class="col-md-12">
+         
+         <div class="card">
+            <div class="card-body">
+            </nav>
+            <div class="text-center">
+               <img src="http://localhost:8000/assets/images/1.png" class="rounded" alt="Gambar" height=900px width=700px>
+            </div>
+            
+            </div>
+         
+      
+      </div>
+   </div>
+
+@endsection
